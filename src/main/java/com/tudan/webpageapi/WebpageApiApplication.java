cb78fbcd0d54b9c089d8efcfba738ac8e1b9ac3b
Mario Tuđan <mario_tudan@msn.com>
2018-02-24 11:27:09 +0100

    Initial commit

---
+++ src/main/java/com/tudan/webpageapi/WebpageApiApplication.java
@@ -0,0 +1,12 @@
+package com.tudan.webpageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpageApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tudan/webpageapi/WebpageApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tudan.webpageapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebpageApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
